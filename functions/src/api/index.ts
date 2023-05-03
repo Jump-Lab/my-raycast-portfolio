9dd1a96ed37f193d078309d9ba74c5806cb56e67
Phong Vu <caophong.w@gmail.com>
2023-05-03 16:37:00 +0700

    More data

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -20,9 +20,8 @@ router.get('/portfolio',async (req, res) => {
 
 router.post('/portfolio',async (req, res) => {
   try {
-    console.log("Log ~ router.post ~ req.body:", req.body)
     //@ts-ignore
-    const result = await addPortfolioItem(req.user.sub, req.body)
+    const result = await addPortfolioItem(req.user, req.body)
     console.log("Log ~ router.post ~ result:", result)
     res.status(200).send();
   } catch (e) {
