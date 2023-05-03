1b9adfa28fe10ee29f5a66f3fa8f576ccd6e3984
Phong Vu <caophong.w@gmail.com>
2023-05-03 16:05:49 +0700

    Firebase integration

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 
-import { getPortfolio } from "../collections"
+import { getPortfolio, addPortfolioItem } from "../collections"
 
 const router = Router();
-
 router.get('/health-check', (req, res) => {
   res.status(200).send('OK')
 })
@@ -12,7 +11,6 @@ router.get('/portfolio',async (req, res) => {
   try {
     //@ts-ignore
     const portfolio = await getPortfolio(req.user.sub)
-    console.log("Log ~ router.get ~ portfolio:", portfolio)
     res.status(200).send(portfolio);
   } catch (e) {
     console.log("Log ~ router.get ~ e:", e)
@@ -22,9 +20,11 @@ router.get('/portfolio',async (req, res) => {
 
 router.post('/portfolio',async (req, res) => {
   try {
+    console.log("Log ~ router.post ~ req.body:", req.body)
     //@ts-ignore
-    const portfolio = await getPortfolio(req.user.uid)
-    res.status(200).send(portfolio);
+    const result = await addPortfolioItem(req.user.sub, req.body)
+    console.log("Log ~ router.post ~ result:", result)
+    res.status(200).send();
   } catch (e) {
     res.status(500);
   }
--- functions/src/collections/index.ts
+++ functions/src/collections/index.ts
@@ -1,4 +1,5 @@
-import { admin } from "../config/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { admin, db } from "../config/firebase";
 
 export enum COLLECTIONS {
   PORTFOLIO_ITEM = "PortfolioItem",
@@ -6,12 +7,25 @@ export enum COLLECTIONS {
 
 export const getPortfolio = async (userId: string) => {
   const data = await admin.firestore().collection(COLLECTIONS.PORTFOLIO_ITEM).where("userId", "==", userId).get();
-  return data.docs.map(item => item.data());
+  return data.docs[0].data();
 };
 
-export const addPortfolioItem = async (item: any) => {
-  return admin
-    .firestore()
-    .collection(COLLECTIONS.PORTFOLIO_ITEM)
-    .add({ ...item, createdAt: admin.firestore.Timestamp.now() });
+export const addPortfolioItem = async (userId: string, portfolio: any) => {
+  const portfolioData = {
+    portfolio,
+    userId,
+    lastUpdated: admin.firestore.Timestamp.now(),
+  };
+  console.log("Log ~ addPortfolioItem ~ portfolioData:", portfolioData)
+  const data = await admin.firestore().collection(COLLECTIONS.PORTFOLIO_ITEM).where("userId", "==", userId).get();
+  console.log("Log ~ addPortfolioItem ~ data:", data);
+  const docs = data.docs;
+  console.log("Log ~ addPortfolioItem ~ data:", docs);
+  if (docs.length) {
+    const docId = docs[0].id;
+    console.log("Log ~ addPortfolioItem ~ docId:", docId);
+    return admin.firestore().collection(COLLECTIONS.PORTFOLIO_ITEM).doc(docId).update(portfolioData);
+  } else {
+    return admin.firestore().collection(COLLECTIONS.PORTFOLIO_ITEM).add(portfolioData);
+  }
 };
